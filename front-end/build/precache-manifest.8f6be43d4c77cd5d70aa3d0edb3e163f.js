3945eecf1528cbb7fb4e060dc8c14dea575994ef
Sachin <sachinkrish7@gmail.com>
2020-11-15 14:51:50 +1100

    added production version of website

---
+++ front-end/build/precache-manifest.8f6be43d4c77cd5d70aa3d0edb3e163f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8cf6e07e167fc2b596824cd0d846dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "253577984256d114cf7d",
+    "url": "/static/css/2.c6d4b46f.chunk.css"
+  },
+  {
+    "revision": "d9995de59d0049d59256",
+    "url": "/static/css/main.3b08a2af.chunk.css"
+  },
+  {
+    "revision": "253577984256d114cf7d",
+    "url": "/static/js/2.aa18b940.chunk.js"
+  },
+  {
+    "revision": "0cd4fe0925810be8f767d6023966f661",
+    "url": "/static/js/2.aa18b940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9995de59d0049d59256",
+    "url": "/static/js/main.d0523ceb.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "0f8454f877f4bfd7a173ae29b868630d",
+    "url": "/static/media/bathtub.0f8454f8.png"
+  },
+  {
+    "revision": "1bd4a050e1367255cc248a4c88e915be",
+    "url": "/static/media/car.1bd4a050.png"
+  },
+  {
+    "revision": "4c3f23559adbd8570d9d31dc0ee3b2fb",
+    "url": "/static/media/drive.4c3f2355.png"
+  },
+  {
+    "revision": "00acd33f4f70588e390bbca0d4e026e4",
+    "url": "/static/media/gavel.00acd33f.png"
+  },
+  {
+    "revision": "2161005c28a9d740893c16fdace5d737",
+    "url": "/static/media/main-home.2161005c.jpg"
+  },
+  {
+    "revision": "cf3bbee89245408d7989f660da978185",
+    "url": "/static/media/police.cf3bbee8.png"
+  },
+  {
+    "revision": "0c82048c1f8ff60c8ee2c2169597bbb1",
+    "url": "/static/media/school.0c82048c.png"
+  },
+  {
+    "revision": "77d79b639ee598bb4e390f37af1f3edf",
+    "url": "/static/media/supermarket.77d79b63.png"
+  },
+  {
+    "revision": "2d733718bd4cb4db180f3cfb42fb9bb8",
+    "url": "/static/media/university.2d733718.png"
+  },
+  {
+    "revision": "5d3b41433b6816badd016de0681bc73c",
+    "url": "/static/media/walking.5d3b4143.png"
+  }
+]);
